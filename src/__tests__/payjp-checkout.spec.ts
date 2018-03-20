f7f123a28ca8b2b8f8efc37b0099e5bec4ecfb60
Atsushi Nagase <a@ngs.io>
2018-03-21 05:47:02 +0900

    Implement props

--- src/__tests__/payjp-checkout.spec.ts
+++ src/__tests__/payjp-checkout.spec.ts
@@ -1,22 +1,47 @@
-import { shallow, Wrapper, ThisTypedShallowOptions } from '@vue/test-utils';
+import { shallow, Wrapper } from '@vue/test-utils';
 import PayjpCheckout from '../';
 
 describe('PayjpCheckout', () => {
   let propsData: () => object,
     component: () => Wrapper<PayjpCheckout>,
-    subject: any;
+    global = window as any;
   beforeEach(() => {
+    delete global.onCreatedPayjpToken_45cfff31a1607c000;
+    delete global.onFailedPayjpToken_3befe4a56b4e64000;
     propsData = () => ({
-      onCreatedCallbackName: "onCreatedPayjpToken_45cfff31a1607c000",
-      onFailedCallbackName: "onFailedPayjpToken_3befe4a56b4e64000"
+      apiKey: 'asdfghjk',
+      clientID: '1234',
+      text: 'pay!',
+      submitText: '"submit!',
+      tokenName: 'test token',
+      previousToken: 'foo',
+      namePlaceholder: 'John Doe',
+      createdCallbackName: 'onCreatedPayjpToken_45cfff31a1607c000',
+      failedCallbackName: 'onFailedPayjpToken_3befe4a56b4e64000'
     });
-    component = () => shallow(PayjpCheckout, {
-      propsData: propsData()
-    });
-    subject = () => component().html();
+    component = () =>
+      shallow(PayjpCheckout, {
+        propsData: propsData()
+      });
   });
 
-  it('renders hello', () => {
-    expect(subject()).toMatchSnapshot();
+  it('matches snapshot', () => {
+    expect(component().html()).toMatchSnapshot();
+  });
+
+  it('fills callback globally', () => {
+    expect(global.onCreatedPayjpToken_45cfff31a1607c000).toBeUndefined();
+    expect(global.onFailedPayjpToken_3befe4a56b4e64000).toBeUndefined();
+    component();
+    expect(global.onCreatedPayjpToken_45cfff31a1607c000).toBeInstanceOf(
+      Function
+    );
+    expect(global.onFailedPayjpToken_3befe4a56b4e64000).toBeInstanceOf(
+      Function
+    );
+    global.onCreatedPayjpToken_45cfff31a1607c000();
+    expect(global.onCreatedPayjpToken_45cfff31a1607c000).toBeUndefined();
+    global.onFailedPayjpToken_3befe4a56b4e64000();
+    expect(global.onFailedPayjpToken_3befe4a56b4e64000).toBeUndefined();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,64 @@
-import { CreateElement, VNode, VueConstructor } from 'vue';
+import { CreateElement, VNode } from 'vue';
 import { Vue, Component, Prop } from 'vue-property-decorator';
 
-function generateCallback(prefix: string, func: () => any): string {
+function generateCallbackName(prefix: string): string {
   const rand = Math.ceil(Math.random() * 1e20).toString(0x10);
   const name = prefix + rand;
   const w = window as any;
   if (w[name]) {
-    return generateCallback(prefix, func);
+    return generateCallbackName(prefix);
   }
-  w[name] = func;
   return name;
 }
 
+const CREATED = 'created',
+  FAILED = 'failed',
+  CREATED_CALLBACK_PREFIX = 'onCreatedPayjpToken_',
+  FAILED_CALLBACK_PREFIX = 'onFailedPayjpToken_';
+
 @Component
 export default class PayjpCheckout extends Vue {
+  /** あなたのパブリックキー */
+  @Prop({ required: true })
+  apiKey!: string;
 
-  @Prop({ type: String, default: '' })
-  createdCallbackName: string = '';
+  /** PAY.JPで発行したOAuth Client ID */
+  @Prop({})
+  clientID?: string;
 
-  @Prop({ type: String, default: '' })
-  failedCallbackName: string = '';
+  /** trueとセットすると、カード情報フォーム入力後に自動的に送信(自動的なトークン作成)しない */
+  @Prop({ default: false })
+  partial!: boolean;
+
+  /** カード情報入力フォームを開くのボタンのテキスト */
+  @Prop({})
+  text?: string;
+
+  /** カード情報入力フォーム内の送信ボタンのテキスト */
+  @Prop({})
+  submitText?: string;
+
+  /** 作成されたトークンがセットされるinput name(hidden) */
+  @Prop({})
+  tokenName?: string;
+
+  /** エラーにより画面に戻ってきた場合などに、再入力をさせないために作成済みのトークンを入れるパラメーター */
+  @Prop({})
+  previousToken?: string;
+
+  /** メッセージなどの表示言語 */
+  @Prop({ default: 'ja', validator: value => /^ja|en$/.test(value) })
+  lang!: string;
+
+  /** カード名義のplaceholderにセットされる値 */
+  @Prop({})
+  namePlaceholder?: string;
+
+  @Prop({})
+  private createdCallbackName!: string;
+
+  @Prop({})
+  private failedCallbackName!: string;
 
   render(createElement: CreateElement): VNode {
     return createElement('div', {});
@@ -27,44 +66,50 @@ export default class PayjpCheckout extends Vue {
 
   created() {
     const emit = this.$emit.bind(this);
-    const onCreated = function (...args: any[]) {
-      emit('created', ...args);
+    const assignCallback = (eventName: string, name: string) => {
+      const w = window as any;
+      w[name] = (...args: any[]) => {
+        emit(eventName, ...args);
+        try {
+          delete w[name];
+        } finally {
+        }
+      };
     };
-    const onFailed = function (...args: any[]) {
-      emit('failed', ...args);
-    };
-    const w = window as any;
-    console.debug(this.createdCallbackName, this.$props.createdCallbackName, "]]");
-    if (this.createdCallbackName) {
-      w[this.createdCallbackName] = onCreated;
-    } else {
-      this.createdCallbackName = generateCallback('onCreatedPayjpToken_', onCreated);
+    if (!this.createdCallbackName) {
+      this.createdCallbackName = generateCallbackName(CREATED_CALLBACK_PREFIX);
     }
-    if (this.failedCallbackName) {
-      w[this.failedCallbackName] = onFailed;
-    } else {
-      this.failedCallbackName = generateCallback('onFailedPayjpToken_', onFailed);
+    assignCallback(CREATED, this.createdCallbackName);
+    if (!this.failedCallbackName) {
+      this.failedCallbackName = generateCallbackName(FAILED_CALLBACK_PREFIX);
     }
+    assignCallback(FAILED, this.failedCallbackName);
   }
 
   mounted() {
     const scriptEl = document.createElement('script');
+    // https://pay.jp/docs/checkout
     const attrs = {
-      'src': 'https://checkout.pay.jp/',
-      'class': 'payjp-button',
-      'data-key': this.$attrs['api-key'],
+      src: 'https://checkout.pay.jp/',
+      class: 'payjp-button',
+      'data-key': this.apiKey,
+      'data-payjp': this.clientID,
+      'data-partial': this.partial ? 'true' : 'false',
+      'data-text': this.text,
+      'data-submit-text': this.submitText,
+      'data-token-name': this.tokenName,
+      'data-previous-token': this.previousToken,
+      'data-lang': this.lang,
       'data-on-created': this.createdCallbackName,
       'data-on-failed': this.failedCallbackName,
-      'data-text': this.$attrs.text,
-      'data-submit-text': this.$attrs['submit-text'],
-      'data-partial': 'true'
-    };
+      'data-name-placeholder': this.namePlaceholder
+    } as any;
     Object.keys(attrs).forEach(key => {
-      const value = (attrs as any)[key];
+      const value = attrs[key];
       if (value !== undefined) {
         scriptEl.setAttribute(key, value);
       }
     });
     this.$el.appendChild(scriptEl);
   }
-};
+}
